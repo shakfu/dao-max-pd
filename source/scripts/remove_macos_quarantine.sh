cefac97b1cf3beb62f4cab3dae9fb7fab136c2f1
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2025-05-08 19:00:35 +0300

    snap

--- source/scripts/remove_macos_quarantine.sh
+++ source/scripts/remove_macos_quarantine.sh
@@ -1,15 +1,19 @@
 echo "\
-It's better to run codesigned and notarized software in general
-for secure software. In the apple ecosystem, this requires the developer
-to have an apple developer account which costs 100 USD per year.
+In general, it's better to run codesigned and notarized software 
+from trusted sources.
 
-If you are building externals yourself, or 
-using github actions where you can transparently read the code that
-is to be built remotely, you can optionally remove the quarantine
-that apple applies to any externally downloaded code which is not
-codesigned and notarized (with apple developer credentials).
+In the Apple ecosystem, in order to become a trusted source for externally
+downloadable software, you have to pay for an Apple developer account which
+costs 100 USD per year, and then use credentials to sign software and get
+your software notarized by Apple.
 
-It does this by the following commandline instructions:
+Any software that is downloaded from external sources is automatically
+put under quarantine by macOS.
+
+If you are building externals yourself this quarantine is not applied.or
+However, if you are using github actions where you can transparently read
+the code that is built remotely, you can optionally choose to remove this
+quarantine by running the following commandline instructions:
 
 codesign --sign - --timestamp --force externals/*.mxo/**/MacOS/*
 /usr/bin/xattr -r -d com.apple.quarantine externals/*.mxo
@@ -29,8 +33,9 @@ That command causes the OS to skip the malware check for the file specified;
 it normally gets removed if it doesn't find any in it. Running it should only
 be done if the computer falsely detects some and it's from a trusted source.
 
-It's up to you to decide if you have built it from a trusted source, so the
-the commands are commented out by default. Uncomment at your own risk.
+Ultimately it's up to you to decide if you are going to trust this software
+that you have downloaded. The commands are commented out by default.
+Uncomment and run them at your own risk.
 "
 # codesign --sign - --timestamp --force externals/*.mxo/**/MacOS/*
 # /usr/bin/xattr -r -d com.apple.quarantine externals/*.mxo
